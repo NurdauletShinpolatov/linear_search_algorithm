148e1eafab69b5587f68568b8b3572a699aa970a
Nurdaulet Shinpolatov <99029072+NurdauletShinpolatov@users.noreply.github.com>
2023-06-13 17:01:33 +0500

    Update cpp.cpp

--- cpp.cpp
+++ cpp.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int linearSearch(int arr[], int size, int target) {
     for (int i = 0; i < size; i++) {
         if (arr[i] == target) {
-            return i; // Return the index where the target element is found
+            return i;
         }
     }
-    return -1; // Return -1 if the target element is not found
+    return -1;
 }
 
 int main() {
