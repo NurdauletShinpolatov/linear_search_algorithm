9bc00bc2b5b055a556418630f6562b1186f213e9
Nurdaulet Shinpolatov <99029072+NurdauletShinpolatov@users.noreply.github.com>
2023-06-13 22:38:49 +0500

    Update python.py

--- python.py
+++ python.py
@@ -1,15 +1,1 @@
-def linear_search(arr, target):
-    for i in range(len(arr)):
-        if arr[i] == target:
-            return i  
-    return -1  
 
-arr = [5, 2, 8, 12, 3]
-target = 8
-
-result = linear_search(arr, target)
-
-if result != -1:
-    print(f"Element found at index {result}")
-else:
-    print("Element not found in the list")
